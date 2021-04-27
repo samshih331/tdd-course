69208ecd69cc0d1c87352381ef54b12e1fef04de
Sam Shih <sam810331@gmail.com>
2021-04-27 15:30:53 +0800

    update

--- src/main/java/budget/Finance.java
+++ src/main/java/budget/Finance.java
@@ -1,9 +1,21 @@
 package budget;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public class Finance {
+
+  BudgetRepo budgetRepo = new BudgetRepoImpl();
+
   public double queryBudget(LocalDate start, LocalDate end) {
+    List<Budget> all = budgetRepo.getAll();
+    for (Budget budget : all) {
+      String yearMonth = budget.getYearMonth();
+      if (yearMonth.equals("202104")) {
+        return budget.getAmount();
+      }
+
+    }
 
     return 0d;
   }
--- src/test/java/budget/BudgetTests.java
+++ src/test/java/budget/BudgetTests.java
@@ -1,5 +1,10 @@
 package budget;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
 /*
 算預算
 某一個月預算多少
@@ -14,4 +19,16 @@ LocalDate
  */
 public class BudgetTests {
 
+  private final Finance finance = new Finance();
+
+  @Test
+  public void queryWholeMonth() {
+    double queryBudget = finance.queryBudget(LocalDate.of(2021, 4, 1), LocalDate.of(2021, 4, 30));
+    Assertions.assertEquals(queryBudget, 310000d);
+  }
+  @Test
+  public void queryWhole2Month() {
+    double queryBudget = finance.queryBudget(LocalDate.of(2021, 4, 1), LocalDate.of(2021, 5, 31));
+    Assertions.assertEquals(queryBudget, 313000d);
+  }
 }
